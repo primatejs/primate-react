7b0313befbbc3d80e034245df6b69584fa2d2a45
terrablue <102580937+terrablue@users.noreply.github.com>
2022-04-27 17:46:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Primate React client
+
+A Primate React handler that can serve as an alternative to the native HTML
+handler.
+
+## Installing
+
+```
+npm install primate-react
+```
+
+## Using
+
+Create a JSX component in `components`, e.g. `PostIndex.jsx`.
+
+```jsx
+import React from "react";
+
+export default class extends React.Component {
+  render() {
+    const {posts} = this.props;
+
+    return (
+      <div>
+        <h1>All posts</h1>
+        {posts.map(({_id, title}) => (
+          <h2><a href={`/react/post/view/${_id}`}>{title}</a></h2>
+        ))}
+      </div>
+    )
+  }
+}
+```
+
+Create a route using the handler.
+
+```js
+import {router, redirect, defined} from "primate";
+import react from "primate-react";
+import Post from "../domains/Post.js";
+
+router.get("/react/posts", () => react`<PostIndex posts="${Post.find()}" />`);
+```
+
+## Resources
+
+[Primate app][primate-app] includes examples for routes and components that are
+1:1 aligned with the HTML examples.
+
+## License
+
+BSD-3-Clause
+
+[primate-app]: https://github.com/primatejs/primate-app
---
+++ exports.js
@@ -0,0 +1,54 @@
+import ReactDOMServer from "react-dom/server";
+import React from "react";
+import {app, DOMParser, http404} from "primate";
+import {File, Path} from "runtime-compat";
+import babel from "@babel/core";
+
+const cwd = new Path(import.meta.url).directory;
+const presets = ["@babel/preset-react"];
+const plugins = [["replace-import-extension", {"extMapping": {".jsx": ".js"}}]];
+const ending = ".jsx";
+
+const {"paths": {"components": path}} = app.conf;
+const jsx_path = `${app.conf.paths.public}/jsx/`;
+if (!await File.exists(jsx_path)) {
+  await File.create(jsx_path);
+}
+const components = {};
+if (await File.exists(path)) {
+  (await Promise.all((await File.list(path))
+    .filter(name => name.endsWith(ending))
+    .map(async name => {
+      const __html = await File.read(`${path}/${name}`);
+      const {code} = babel.transformSync(__html, {cwd, presets, plugins});
+      const write_path = `${jsx_path}/${name.slice(0, -ending.length)}.js`;
+      await File.write(write_path, code);
+      return {name, write_path};
+    })))
+    .forEach(async ({name, write_path}) => {
+      components[name.slice(0, -4)] = (await import(write_path)).default;
+    });
+}
+
+const render = (component, attributes) =>
+  ReactDOMServer.renderToString(React.createElement(component, attributes));
+
+const last = -1;
+export default async (strings, ...keys) => {
+  const tag = (await DOMParser.parse(strings
+    .slice(0, last)
+    .map((string, i) => `${string}$${i}`)
+    .join("") + strings[strings.length+last], await Promise.all(keys)))
+    .children[0];
+  const {tag_name, attributes} = tag;
+  if (components[tag_name] !== undefined) {
+    const component = components[tag_name];
+    const html = render(component, attributes);
+    const body = app.index.replace("<body>", () => `<body>${html}`);
+    const code = 200;
+    const headers = {"Content-Type": "text/html"};
+    return {body, code, headers};
+  } else {
+    return http404;
+  }
+};
